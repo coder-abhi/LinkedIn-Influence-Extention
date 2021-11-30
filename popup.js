2bb138579d5df9a0303f82b3295b34601f276928
coder-abhi <abhi.kamble.3987@gmail.com>
2021-11-30 15:43:52 +0530

    Improving HTML

--- popup.js
+++ popup.js
@@ -1,10 +1,16 @@
+// Required buttons are selected from popup.html
 startBtn = document.getElementById('newBtn');
 counterEle = document.getElementById('reqCounter');
+
+// Eventlistener for Button
 startBtn.addEventListener('click', () => {
+    startBtn.innerText = "In Progress..."
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { process: { status: "start" } });
     });
 })
+
+//Event listener for Reciving messages
 chrome.runtime.onMessage.addListener(({ process }, sender, sendResponse) => {
     if (process.status === "ready") {
         startBtn.innerText = "Start"
